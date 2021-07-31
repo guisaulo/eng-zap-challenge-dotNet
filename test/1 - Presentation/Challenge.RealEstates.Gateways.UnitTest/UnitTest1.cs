fce3c2fd37336bbed20ec5299c20f67e549adcde
Guilherme Saulo Alves <guisaulo@hotmail.com>
2021-07-30 22:15:30 -0300

    testes

---
+++ test/1 - Presentation/Challenge.RealEstates.Gateways.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Challenge.RealEstates.Gateways.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
