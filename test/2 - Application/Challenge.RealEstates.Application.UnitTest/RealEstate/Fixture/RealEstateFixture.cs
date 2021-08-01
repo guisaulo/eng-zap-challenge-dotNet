5ad5d30dec0e06d7884c18aa186704274d70131b
Guilherme Saulo Alves <guisaulo@hotmail.com>
2021-08-01 09:24:17 -0300

    Teste unitário application service

---
+++ test/2 - Application/Challenge.RealEstates.Application.UnitTest/RealEstate/Fixture/RealEstateFixture.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Challenge.RealEstates.Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Challenge.RealEstates.Application.Mappers;
+using Challenge.RealEstates.Domain.DomainResponse;
+using Challenge.RealEstates.Domain.Filter;
+using Challenge.RealEstates.Domain.PagedParam;
+using Moq;
+
+namespace Challenge.RealEstates.Application.UnitTest.RealEstate.Fixture
+{
+    public class RealEstateFixture
+    {
+        public readonly RealEstateApplicationService Service;
+
+        public RealEstateFixture()
+        {
+            Service = new RealEstateApplicationService(InitializeRealEstateService(), InitializeRealEstateAutoMapper());
+        }
+
+        private static IRealEstateService InitializeRealEstateService()
+        {
+            var mock = new Mock<IRealEstateService>();
+
+            mock.Setup(m => m.AddRange(It.IsAny<IEnumerable<Domain.Entities.RealEstate>>()))
+                .Returns(new AddRangeResponse());
+            return mock.Object;
+
+            mock.Setup(m => m.GetAllPaged(It.IsAny<PagedParams>(), It.IsAny<Filters>()))
+                .Returns((PagedParams p, Filters f) => GetRealEstates(p, f));
+            return mock.Object;
+
+            throw new NotImplementedException();
+        }
+
+        private static PagedResponse<Domain.Entities.RealEstate> GetRealEstates(PagedParams pagedParams, Filters filters)
+        {
+            var realEstate = new Domain.Entities.RealEstate();
+            return new PagedResponse<Domain.Entities.RealEstate>()
+            {
+                PageNumber = pagedParams.PageNumber,
+                PageSize = pagedParams.PageSize,
+                TotalCount = 1,
+                Listings = new List<Domain.Entities.RealEstate>() { realEstate }
+            };
+        }
+
+        private static IMapper InitializeRealEstateAutoMapper()
+        {
+            var config = new MapperConfiguration(opts => { opts.AddMaps(Assembly.GetAssembly(typeof(RealEstateProfile))); });
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ test/2 - Application/Challenge.RealEstates.Application.UnitTest/RealEstate/RealEstateApplicationServiceUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Challenge.RealEstates.Application.DTOs;
+using Challenge.RealEstates.Application.UnitTest.RealEstate.Fixture;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Challenge.RealEstates.Application.UnitTest.RealEstate
+{
+    public class RealEstateApplicationServiceUnitTest : IClassFixture<RealEstateFixture>
+    {
+        private readonly RealEstateApplicationService _realEstateApplicationService;
+
+        public RealEstateApplicationServiceUnitTest(RealEstateFixture fixture)
+        {
+            _realEstateApplicationService = fixture.Service;
+        }
+
+        [Fact]
+        public void Should_AddRange_Successfully()
+        {
+            var realEstate = new List<Domain.Entities.RealEstate>() { new (){ Id = "123" } };
+            var result = _realEstateApplicationService.AddRange(realEstate);
+            result.Should().NotBeNull();
+            Assert.True(result.Input.ValidIds.Count.Equals(0));
+            Assert.True(result.Input.InvalidIds.Count.Equals(0));
+            Assert.True(result.Zap.ValidIds.Count.Equals(0));
+            Assert.True(result.Zap.InvalidIds.Count.Equals(0));
+            Assert.True(result.VivaReal.ValidIds.Count.Equals(0));
+            Assert.True(result.VivaReal.InvalidIds.Count.Equals(0));
+        }
+
+        [Fact]
+        public void Should_GetAllPaged_Successfully()
+        {
+            var result = _realEstateApplicationService.GetAllPaged("zap", new RealEstatesSearchDto());
+            Assert.True(result.PageNumber.Equals(1));
+            Assert.True(result.PageSize.Equals(10));
+            Assert.True(result.TotalCount.Equals(1));
+            Assert.True(result.Listings.Count().Equals(1));
+        }
+    }
+}
